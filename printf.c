841a809471f2981ff240068e6711bb966b76154f
dechacsiny duch <2027duch.dechacsin@fcboe.org>
2024-01-17 14:48:16 -0500

    Finished

--- printf.c
+++ printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>	// The stdio.h library contains input and output functions
+#include <stdio.h>
 
 int main()
 {
@@ -7,9 +7,8 @@ int main()
 	char c = 'z';
 
 	// Format specifiers are placeholders for variable data
-	printf("The value of a is: %i\n", a);	// %i is for integers
-	printf("The value of f is: %f\n", f);	// %f is for floating point data
-	printf("The value of c is: %c\n", c);   // %c is for single characters
+	printf("a = %i, f= %f, c = %c\n", a, f, c);  // %i is for integers
 
 	return 0;
+
 }
